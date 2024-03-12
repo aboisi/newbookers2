378b8e0b90797ed5b1f2411481af7f6e03072f54
EC2 Default User <ec2-user@ip-172-31-43-93.ap-northeast-1.compute.internal>
2024-03-12 10:17:14 +0000

    aboutページ作成

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,20 @@
 class ApplicationController < ActionController::Base
   #devise利用の機能（ユーザ登録、ログイン認証など）が使われる前にconfigure_permitted_parametersメソッドが実行される
   before_action :configure_permitted_parameters, if: :devise_controller?
-  
-  #privateは記述したコントローラ内でしか参照できないが、protectedは呼び出された他のコントローラからも参照
+
+  #サインイン後の遷移先
+  def after_sign_in_path_for(resource)
+    about_path
+  end
+
+  #サインアウト後の遷移先
+  def after_sign_out_path_for(resource)
+    about_path
+  end
+
+  #privateは記述したコントローラ内でしか参照できないが、protectedは呼び出された他のコントローラからも参照できる
   protected
-  
+
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
   end
